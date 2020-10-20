14f65eeabd1bcdd7fedc0c72704ff7e05aa8402c
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-10-20 11:19:11 -0500

    added sort functionality

--- app.js
+++ app.js
@@ -7,7 +7,33 @@ app.use(morgan('common'));
 const playStore = require('./playstore');
 
 app.get('/apps', (req, res) => {
+  const {sort} = req.query;
   const returnData = [...playStore];
+
+  // sort validation
+  if(sort && sort !== 'rating' && sort !== 'app') {
+    res
+      .status(400)
+      .send('Sort must be one of rating or app');
+  }
+  // sort by rating or app
+  if(sort) {
+    let newSort;
+
+    if(sort === 'rating'){
+      newSort = 'Rating';
+    } else {
+      newSort = 'App';
+    }
+    
+    returnData.sort((currApp, nextApp) => {
+      console.log('currApp rating', currApp[newSort]);
+      if(currApp[newSort] > nextApp[newSort]) return 1;
+      if(currApp[newSort] < nextApp[newSort]) return -1;
+      return;
+    });
+  }
+  if(sort === 'app')
 
   res.json(returnData);
 });
