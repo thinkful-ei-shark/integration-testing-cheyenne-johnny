b89379929c13a8cd7e6f99d358d680a769b9a5e6
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-10-20 10:55:55 -0500

    added playstore.js

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-
 app.use(morgan('common'));
+
+const playStore = require('./playstore');
+
+app.get('/apps', (req, res) => {
+  const returnData = [...playStore];
+
+  res.json(returnData);
+});
 
 app.listen(8080, () => console.log('server started'));
